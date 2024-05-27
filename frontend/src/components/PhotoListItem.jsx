b47abc06c4bcd0562a5355c57fdded137651e355
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-05-27 14:37:37 -0230

    Built the PhotoListItem component to display individual photo details including the main photo, photographer's profile picture, username, and location. Ensured images are stacked vertically with proper spacing. Integrated the component into the App component using sample data for initial rendering. Prepared for future enhancements by setting up the structure to accept props for dynamic data.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+
+const { sampleDataForPhotoListItem } = props
+
+  return (
+    <article className="item">
+      <img src={sampleDataForPhotoListItem.imageSource} />
+      <img src={sampleDataForPhotoListItem.profile} />
+      <p>{sampleDataForPhotoListItem.username}</p>
+      <span>{sampleDataForPhotoListItem.location.city}</span>
+      {" "}
+      <span>{sampleDataForPhotoListItem.location.country}</span>
+    </article>
+  );
 };
 
 export default PhotoListItem;
