c1a91dcbbdf0547b5855fd8b0e48d41765535d86
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-05-28 13:26:23 -0230

    changed favicon to favbadge as per AI feedback

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import TopicList from './TopicList';
-import FavIcon from './FavIcon';
+// import FavIcon from './FavIcon';
+import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss'
 
-const selected = true
+// const selected = true
 
 const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList />
-      <FavIcon selected={selected} />
+      <FavBadge />
     </div>
   )
 }
