07de4d7f51f7cd955fe4522e5d19b18d234128a2
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-05-28 16:58:42 -0230

    Introduced and integrated mock data for photos and topics in HomeRoute. Imported mock data from 'frontend/src/mocks/photos.js' and 'frontend/src/mocks/topics.js', and updated App.jsx to consume these files, replacing the default props previously used to simulate component data. Removed default props from the components as part of a code cleanup, ensuring the application now relies on the new mock data for rendering.

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -4,11 +4,14 @@ import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss'
 
 
-const TopNavigation = () => {
+const TopNavigation = (props) => {
+
+const { topics } = props
+
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList />
+      <TopicList topics={topics} />
       <FavBadge />
     </div>
   )
--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -3,38 +3,26 @@ import React from "react";
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
 
-const topics = sampleDataForTopicList.map(topic => {
-return (
-  <TopicListItem
-key={topic.id}
-slug={topic.slug}
-title={topic.title}
-  />
-)
-})
 
-const TopicList = () => {
+
+const TopicList = (props) => {
+
+  const { topics } = props 
+
+  const topicsArr = topics.map(topic => {
+    return (
+      <TopicListItem
+    key={topic.id}
+    slug={topic.slug}
+    title={topic.title}
+      />
+    )
+    })
+
   return (
     <div className="top-nav-bar__topic-list">
-      {topics}
+      {topicsArr}
     </div>
   );
 };
