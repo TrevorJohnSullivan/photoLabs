451485eba34bfa0c1be4cf4510e09d84672e25ae
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-06-05 11:47:00 -0230

    made changes so liking photo in modal also likes photo on main page

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,18 +4,16 @@ import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton(props) {
   const { favorites, handlingFavorites, photoId } = props;
-  const [selected, setSelected] = useState(favorites.includes(photoId));
 
   const showSelected = (e) => {
     e.stopPropagation(); 
-    setSelected(!selected);
     handlingFavorites(photoId);
   };
 
   return (
     <div onClick={showSelected} className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        <FavIcon selected={selected} />
+        <FavIcon selected={favorites.includes(photoId)} />
       </div>
     </div>
   );
